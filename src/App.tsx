4106ef9392acc513d7e9385ae1c6a0f32b515822
ElodieMons <emons47110@gmail.com>
2024-10-30 10:08:57 +0100

    install RR & modifs

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,19 @@
-import { useState } from "react";
+import { Link, Outlet } from "react-router-dom";
 import "./App.css";
-
-// pages components
-
-import Home from "./pages_cpnts/Home";
-import About from "./pages_cpnts/About";
-
-
 
 
 // the App
 
 function App() {
-  const [currentLocation, setCurrentLocation] = useState("/");
 
   return (
     <>
       <nav>
-        <button onClick={() => setCurrentLocation("/")} type="button">Home</button>
-        <button onClick={() => setCurrentLocation("/about")} type="button">About</button>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
       </nav>
-      <main>
-        {currentLocation === "/" && <Home />}
-        {currentLocation === "/about" && <About />}
+      <main> 
+        <Outlet/>
       </main>
     </>
   );
